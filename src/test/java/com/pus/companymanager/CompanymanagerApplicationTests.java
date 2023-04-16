18618e9016b3e27e40549ede2df82d328992c4fa
DabliuOjtek <matrix890098@gmail.com>
2023-04-16 11:58:29 +0200

    Init commit

---
+++ src/test/java/com/pus/companymanager/CompanymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pus.companymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
