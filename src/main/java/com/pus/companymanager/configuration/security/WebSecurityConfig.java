109131ec7371c5fed296c242f4ca9a537864a190
Dominik <dominik.rok4@gmail.com>
2023-05-20 11:30:00 +0200

    http config

--- src/main/java/com/pus/companymanager/configuration/security/WebSecurityConfig.java
+++ src/main/java/com/pus/companymanager/configuration/security/WebSecurityConfig.java
@@ -1,18 +1,31 @@
 package com.pus.companymanager.configuration.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final AuthEntryPoint authEntryPoint;
+    private final AuthTokenFilter authTokenFilter;
+    private final CustomLogoutHandler customLogoutHandler;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -20,11 +33,34 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()
-                );
-        return http.build();
+        return http.cors().and().csrf().disable()
+                .authorizeHttpRequests(this::configHttp)
+                .build();
+    }
+
+    private LogoutConfigurer<HttpSecurity> configHttp(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authz) {
+        try {
+            return authz
+                    .anyRequest().permitAll()
+                    .and()
+                    .exceptionHandling().authenticationEntryPoint(authEntryPoint)
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                    .formLogin().disable()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .addLogoutHandler(customLogoutHandler)
+                    .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    protected AuthenticationManager configure(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
