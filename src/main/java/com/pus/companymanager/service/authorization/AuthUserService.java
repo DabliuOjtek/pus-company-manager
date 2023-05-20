67ff64bb21663007ffe120a80119befcad96fd45
DabliuOjtek <matrix890098@gmail.com>
2023-05-20 12:04:13 +0200

    tokens - add condition to loadUserByUsername

--- src/main/java/com/pus/companymanager/service/authorization/AuthUserService.java
+++ src/main/java/com/pus/companymanager/service/authorization/AuthUserService.java
@@ -1,9 +1,11 @@
 package com.pus.companymanager.service.authorization;
 
 import com.pus.companymanager.configuration.security.UserDetailsImpl;
+import com.pus.companymanager.exception.DefaultException;
 import com.pus.companymanager.model.user.User;
 import com.pus.companymanager.repository.user.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,6 +23,11 @@ public class AuthUserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Błędne dane uwierzytelniające"));
+
+        if (!user.isActive()) {
+            throw new DefaultException("Błędne dane uwierzytelniające", HttpStatus.UNAUTHORIZED);
+        }
+
         return UserDetailsImpl.build(user);
     }
 }
