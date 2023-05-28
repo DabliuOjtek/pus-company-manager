1d65a02c3ab07eb8b7535594a0aa806c320802f4
DabliuOjtek <matrix890098@gmail.com>
2023-05-28 12:15:16 +0200

    crud - add method to MemberRepository

--- src/main/java/com/pus/companymanager/service/project/MemberService.java
+++ src/main/java/com/pus/companymanager/service/project/MemberService.java
@@ -1,7 +1,12 @@
 package com.pus.companymanager.service.project;
 
+import com.pus.companymanager.configuration.security.UserDetailsImpl;
+import com.pus.companymanager.exception.DefaultException;
+import com.pus.companymanager.model.user.User;
 import com.pus.companymanager.repository.project.MemberRepository;
+import com.pus.companymanager.service.authorization.user.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +14,12 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private MemberRepository memberRepository;
+    private UserService userService;
+
+    public void isMemberOfProject(Long projectId, UserDetailsImpl userDetails) {
+        User user = userService.getUser(userDetails);
+        if (!memberRepository.existsByUserIdAndProjectId(projectId, user.getId())) {
+            throw new DefaultException("Użytkownik nie należy do projektu", HttpStatus.NOT_FOUND);
+        }
+    }
 }
